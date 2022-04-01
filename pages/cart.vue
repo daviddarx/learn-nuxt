91c69a6b45d0411eb35313c9fbfd9a0d51fad12f
David Darx <daviddarx@gmail.com>
2022-04-01 14:27:24 +0200

    feat(cart) create page, add it in AppMenu

---
+++ pages/cart.vue
@@ -0,0 +1,13 @@
+<template>
+    <main class="container cart">
+        <h2>Cart</h2>
+    </main>
+</template>
+
+<script>
+export default {
+    name: 'Cart',
+}
+</script>
+
+<style lang="scss" scoped></style>
