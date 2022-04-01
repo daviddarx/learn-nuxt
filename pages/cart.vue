51d2771315d0bc55bc1ecf4b0adf934c3ad31b68
David Darx <daviddarx@gmail.com>
2022-04-01 15:31:00 +0200

    feat(cart) adds conditional rendering if cart is empty

--- pages/cart.vue
+++ pages/cart.vue
@@ -1,39 +1,51 @@
 <template>
     <main class="container cart">
-        <h2>Cart</h2>
-        <table>
-            <thead>
-                <tr>
-                    <th>Item</th>
-                    <th>Add Ons</th>
-                    <th>Amount</th>
-                    <th>Total Price</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="item in cart" :key="item.id">
-                    <td>
-                        {{ item.item }}
-                        <span v-if="item.options">- {{ item.options }}</span>
-                    </td>
-                    <td>
-                        <span
-                            v-for="addon in item.addOns"
-                            :key="addon"
-                            class="comma"
-                        >
-                            {{ addon }}</span
-                        >
-                    </td>
-                    <td>{{ item.count }}</td>
-                    <td>{{ item.combinedPrice }}</td>
-                </tr>
-                <tr>
-                    <td colspan="3"></td>
-                    <td class="total">Total: ${{ totalPrice.toFixed(2) }}</td>
-                </tr>
-            </tbody>
-        </table>
+        <div v-if="totalCount > 0">
+            <h2>Cart</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Item</th>
+                        <th>Add Ons</th>
+                        <th>Amount</th>
+                        <th>Total Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="item in cart" :key="item.id">
+                        <td>
+                            {{ item.item }}
+                            <span v-if="item.options"
+                                >- {{ item.options }}</span
+                            >
+                        </td>
+                        <td>
+                            <span
+                                v-for="addon in item.addOns"
+                                :key="addon"
+                                class="comma"
+                            >
+                                {{ addon }}</span
+                            >
+                        </td>
+                        <td>{{ item.count }}</td>
+                        <td>{{ item.combinedPrice }}</td>
+                    </tr>
+                    <tr>
+                        <td colspan="3"></td>
+                        <td class="total">
+                            Total: ${{ totalPrice.toFixed(2) }}
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div v-else class="empty-cart">
+            <h2>Your Cart is empty</h2>
+            <nuxt-link to="/restaurants">
+                <button>Fill it up!</button>
+            </nuxt-link>
+        </div>
     </main>
 </template>
 
@@ -44,9 +56,17 @@ export default {
     name: 'Cart',
     computed: {
         ...mapState(['cart']),
-        ...mapGetters(['totalPrice']),
+        ...mapGetters(['totalPrice', 'totalCount']),
     },
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.empty-cart {
+    text-align: center;
+
+    h2 {
+        margin-bottom: 2em;
+    }
+}
+</style>
