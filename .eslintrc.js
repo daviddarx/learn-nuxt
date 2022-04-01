856739caff5f0a1de7e352b666e1575b669399a0
David Darx <daviddarx@gmail.com>
2022-04-01 08:53:08 +0200

    feat(eslint) update rules with console.log warnings off

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,19 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true,
-  },
-  parserOptions: {
-    parser: '@babel/eslint-parser',
-    requireConfigFile: false,
-  },
-  extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier'],
-  plugins: [],
-  // add your custom rules here
-  rules: {
-    'vue/multi-word-component-names': 'off',
-  },
+    root: true,
+    env: {
+        browser: true,
+        node: true,
+    },
+    parserOptions: {
+        parser: '@babel/eslint-parser',
+        requireConfigFile: false,
+    },
+    extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier'],
+    plugins: [],
+    // add your custom rules here
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'vue/multi-word-component-names': 'off',
+    },
 }
