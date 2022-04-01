7a8e1c1f0ebdc534c7237364d6efe1b34e3d5993
David Darx <daviddarx@gmail.com>
2022-04-01 10:38:37 +0200

    feat(restaurants) adds filtering logic with computed properties

--- pages/restaurants.vue
+++ pages/restaurants.vue
@@ -4,15 +4,17 @@
             <h1>Restaurants</h1>
             <AppSelect
                 :title="'What cuisine do you want?'"
+                :options="filteringOptions"
                 @change="filterRestaurant"
             />
         </div>
 
-        <AppRestaurantInfo />
+        <AppRestaurantInfo :datasource="filteredRestaurant" />
     </main>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 import AppRestaurantInfo from '@/components/AppRestaurantInfo.vue'
 import AppSelect from '@/components/AppSelect.vue'
 
@@ -25,7 +27,21 @@ export default {
     data() {
         return {
             selectedRestaurant: '',
+            filteringOptions: ['tacos', 'pizza', 'dim sum'],
         }
+    },
+    computed: {
+        ...mapState(['fooddata']),
+        filteredRestaurant() {
+            if (this.selectedRestaurant) {
+                return this.fooddata.filter((el) => {
+                    const name = el.name.toLowerCase()
+
+                    return name.includes(this.selectedRestaurant)
+                })
+            }
+            return this.fooddata
+        },
     },
     methods: {
         filterRestaurant(restaurant) {
